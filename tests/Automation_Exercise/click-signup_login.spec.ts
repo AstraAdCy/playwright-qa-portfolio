5538cf796195851af694491c5b7cc06aa3fc38c8
AstraAdCy <adastra2200@gmail.com>
2026-06-11 09:36:51 +0100

    Week 1 Day 3 - Google title test and automationexercise signup test

---
+++ tests/Automation_Exercise/click-signup_login.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('To Verify signup/login page loading', async ({ page }) => {
+  await page.goto('https://automationexercise.com');
+  await page.getByRole('button', {name: 'consent'}).click();
+  await page.getByRole('link', {name: 'Signup / Login'}).click();
+  await expect(page).toHaveURL('https://automationexercise.com/login');
+  await expect(page.getByText('Login to your account')).toBeVisible();
+});
